3e00bbabd40c8aa357da101b8e472e4d4abb714c
Flake212 <111091522+Flake2121@users.noreply.github.com>
2024-05-21 19:58:09 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+  var w = window.innerWidth;
+  var h = window.innerHeight;
+  createCanvas(w, h);
+  background(255);
+  point(40, 40);
+  frameRate(20);
+}
+
+function draw() {
+  if (mouseIsPressed) {
+    for (var i = 0; i <= height; i++) {
+      // noprotect
+      for (var j = 0; j <= width; j++) {
+        // noprotect
+        stroke((mouseX / width) * 255, (j / height) * 255, (i / height) * 255);
+        rect(i, j, 1, 1);
+      }
+    }
+  }
+}
